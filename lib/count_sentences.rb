a0d09d482594029e3c4ea0bec3a99804b7b4f040
katherine <katherinejc103@gmail.com>
2022-09-26 09:47:30 -0400

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,21 @@ require 'pry'
 
 class String
 
-  def sentence?
-    
+  def sentence? 
+    self.end_with?(".") 
   end
 
   def question?
-
+    self.end_with?("?")  
   end
 
   def exclamation?
-
+    self.end_with?("!")  
   end
 
   def count_sentences
-
+    sentence_split = self.split(/\?|\.|\!/)
+    puts sentence_split
+    sentence_split.filter{|sentence|!sentence.empty?}.size
   end
 end
